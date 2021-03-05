97da366593cb4e3b62c239a0e7173a89730c0a16
SophiaSamera <Sophia.samera@ocsbstudent.ca>
2021-03-05 00:49:51 +0000

    tried fixing it again

--- script.js
+++ script.js
@@ -7,18 +7,18 @@ function game () {
   points = parseInt(points)
   point = parseInt(point)
   points = points + 1
-  const point = 150
+  point = 150 - points
 
   for (points = 0; points < 150; point++) {
     document.getElementById('need').innerHTML = 'You still need ' + point + ' to complete the game'
-  if (points == 50) {
-  document.getElementById('cinnabun').style.backgroundImage = "url('./Images/dress.png')"
-  } else if (points == 100) {
-    document.getElementById('cinnabun').style.backgroundImage = "url('./Images/bubble.png')"
-  } else if (points == 150) {
-    document.getElementById('cinnabun').style.backgroundImage = "url('./Images/toy.png')"
-  } else {
-    document.getElementById('great').innerHTML = 'Great job!'
+    if (points === 50) {
+      document.getElementById('cinnabun').style.backgroundImage = "url('./Images/dress.png')"
+      } else if (points === 100) {
+        document.getElementById('cinnabun').style.backgroundImage = "url('./Images/bubble.png')"
+        } else if (points === 150) {
+          document.getElementById('cinnabun').style.backgroundImage = "url('./Images/toy.png')"
+          } else {
+            document.getElementById('great').innerHTML = 'Great job!'
     }
   }
 }
