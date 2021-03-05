56c11cbe9d2182cdea4ced3515f538b7f4819b8d
SophiaSamera <Sophia.samera@ocsbstudent.ca>
2021-03-05 01:12:53 +0000

    tried to fix again

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
 document.getElementById('cinnabun').addEventListener('click', game)
 
+document.getElementById('what').addEventListener('click', count)
+
 let points = 0
 let point = 0
 
 function game () {
   points = parseInt(points)
-  point = parseInt(point)
   points = points + 1
+  document.getElementById('points').innerHTML = points
+  if (points === 50) {
+    document.getElementById('cinnabun').style.backgroundImage = "url('./Images/dress.png')"
+  } else if (points === 100) {
+    document.getElementById('cinnabun').style.backgroundImage = "url('./Images/bubble.png')"
+  } else if (points === 150) {
+    document.getElementById('cinnabun').style.backgroundImage = "url('./Images/toy.png')"
+  } else {
+    document.getElementById('great').innerHTML = 'Great job!'
+  }
+}
+
+function count () {
+  document.getElementById('points').innerHTML = points
+  points = parseInt(points)
+  point = parseInt(point)
   point = 150 - points
 
-  for (points = 0; points < 150; point++) {
-    document.getElementById('need').innerHTML = 'You still need ' + point + ' to complete the game'
-    if (points === 50) {
-      document.getElementById('cinnabun').style.backgroundImage = "url('./Images/dress.png')"
-      } else if (points === 100) {
-        document.getElementById('cinnabun').style.backgroundImage = "url('./Images/bubble.png')"
-        } else if (points === 150) {
-          document.getElementById('cinnabun').style.backgroundImage = "url('./Images/toy.png')"
-          } else {
-            document.getElementById('great').innerHTML = 'Great job!'
-    }
+  for (let points = 0; points < 150; point++) {
+    document.getElementById('need').innerHTML = 'You still need' + point + 'to complete the game'
   }
 }
